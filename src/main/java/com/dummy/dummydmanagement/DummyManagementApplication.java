92e3f8d866cf8b07ebd42e79579a3856a2d659b8
Shashwat Singh <Shashwat.Singh@go-mmt.com>
2021-06-16 13:48:29 +0530

    xyz

--- src/main/java/com/dummy/dummydmanagement/DummyManagementApplication.java
+++ src/main/java/com/dummy/dummydmanagement/DummyManagementApplication.java
@@ -1,5 +1,6 @@
 package com.dummy.dummydmanagement;
 
+import com.jayway.blog.YourAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import java.util.concurrent.Executors;
 @SpringBootApplication
 public class DummyManagementApplication {
 
+	@YourAnnotation(isRun = true)
 	public static void main(String[] args) {
 		SpringApplication.run(DummyManagementApplication.class, args);
 	}

28d6006a3eea9194ef5b3d2c2d4e36159922c63e
Alexander Kriegisch <Alexander@Kriegisch.name>
2021-06-17 09:15:01 +0700

    Add AspectJ Weaver dependency, remove system scope from aspect library

--- src/main/java/com/dummy/dummydmanagement/DummyManagementApplication.java
+++ src/main/java/com/dummy/dummydmanagement/DummyManagementApplication.java
@@ -1,20 +1,23 @@
 package com.dummy.dummydmanagement;
 
-import com.jayway.blog.YourAnnotation;
+import com.dummy.dummydmanagement.service.ItemService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.concurrent.Executors;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.dummy.dummydmanagement", "com.jayway.blog"})
 public class DummyManagementApplication {
 
-	@YourAnnotation(isRun = true)
-	public static void main(String[] args) {
-		SpringApplication.run(DummyManagementApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext context = SpringApplication.run(DummyManagementApplication.class, args);
+		context.getBean(ItemService.class).getAllItemsService();
 	}
 
 	@Bean
